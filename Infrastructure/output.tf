117de7534d93cb3ac1bc0a260e3e3a7e2890978a
NourrAhmed <nourahmed.2164@gmail.com>
2025-08-09 21:43:27 +0300

    add infrastructure

---
+++ Infrastructure/output.tf
@@ -0,0 +1,3 @@
+output "instance_ip" {
+  value = aws_instance.CraveCart.public_ip
+}
---
+++ Infrastructure/variables.tf
@@ -0,0 +1,9 @@
+variable "region" {
+  default = "eu-central-1"
+}
+variable "instance_ami" {
+  default = "ami-02003f9f0fde924ea"
+}
+variable "instance_type" {
+  default = "t2.micro"
+}
