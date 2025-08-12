f956f816302c9da03a85cc3cc63ebb67e91e7b45
NourrAhmed <nourahmed.2164@gmail.com>
2025-08-12 16:11:37 +0300

    backend.tf

--- Infrastructure/output.tf
+++ Infrastructure/output.tf
@@ -2,8 +2,8 @@ output "instance_ip" {
   value = aws_instance.CraveCart.public_ip
 }
 output "bucket_name" {
-  value = aws_s3_bucket.StateFileBucket.bucket
+  value = aws_s3_bucket.CraveCartStateFileBucket.bucket
 }
 output "dynamodb_table_name" {
-  value = aws_dynamodb_table.StateLock.name
+  value = aws_dynamodb_table.CraveCartStateLock.name
 }
