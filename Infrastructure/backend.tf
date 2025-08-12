6c9a0b2e01c5e83f166aba0defda893438b1aab5
NourrAhmed <nourahmed.2164@gmail.com>
2025-08-12 13:15:21 +0300

    testing workflow

--- Infrastructure/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "state-bucket"
-    key            = "terraform.tfstate"
-    region = "eu-north-1"
-    dynamodb_table = "stateLock"
-    encrypt        = true
-  }
-}
