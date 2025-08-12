d0e279d01b717e6f46040e9e2499834171439ad2
NourrAhmed <nourahmed.2164@gmail.com>
2025-08-12 13:54:22 +0300

    adding backend.tf

---
+++ Infrastructure/backend.tf
@@ -0,0 +1,9 @@
+terraform {
+  backend "s3" {
+    bucket         = "state-bucket"
+    key            = "terraform.tfstate"
+    region = "eu-north-1"
+    dynamodb_table = "stateLock"
+    encrypt        = true
+  }
+}
