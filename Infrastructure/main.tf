992279a47a97d448c33895816ceb283bc8284232
NourrAhmed <nourahmed.2164@gmail.com>
2025-08-14 11:17:24 +0300

    fixing blue-green deployment

--- Infrastructure/main.tf
+++ Infrastructure/main.tf
@@ -164,6 +164,7 @@ resource "aws_lb_target_group" "blue" {
     timeout             = 5
     healthy_threshold   = 2
     unhealthy_threshold = 2
+    port                = "80"
   }
 }
 
@@ -172,6 +173,17 @@ resource "aws_lb_target_group" "green" {
   port     = 80
   protocol = "HTTP"
   vpc_id   = aws_vpc.main.id
+
+  health_check {
+    path                = "/"
+    protocol            = "HTTP"
+    matcher             = "200"
+    interval            = 30
+    timeout             = 5
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+    port                = "80"
+    }
 }
 
 resource "aws_lb_listener" "http" {
